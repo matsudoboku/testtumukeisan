9bbd243453b4fec2f40db9137242781f1d421cc0
matsudoboku <muramatsu.matsukawa@gmail.com>
2025-07-07 22:47:24 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'tmt-cache-v3';
+const URLS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './privacy.html',
+  './contact.html'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
